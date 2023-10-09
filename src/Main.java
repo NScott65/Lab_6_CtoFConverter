618555a1e0843507d62e8fbd8070e186bb909d19
Noah <scott.613865@swl.k12.oh.us>
2023-10-09 09:21:33 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+
+        //Variable
+        double f = 0;
+        double c = 0;
+        boolean done = false;
+
+        do{
+            System.out.println("Enter the degrees in fahrenheit: ");
+            if(scan.hasNextDouble()){
+                f = scan.nextDouble();
+
+                c = ((f -32) * 5) / 9;
+
+                System.out.printf("%-12s%8.2f", "Celsius:", c);
+                System.out.printf("\n%-12s%8.2f", "Fahrenheit:", f);
+
+                done = true;
+
+            }else{
+                System.out.println("Your input was invalid. Try again.");
+            }
+        }while(!done);
     }
 }
